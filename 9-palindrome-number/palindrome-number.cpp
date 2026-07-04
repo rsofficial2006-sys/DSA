8d2a589e40c00c689e0e6724b9745b2ff283144c
rsofficial2006-sys <rsofficial2006@gmail.com>
2026-07-04 23:22:12 +0530

    Time: 7 ms (14.38%) | Memory: 8.6 MB (37.96%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(int x) {
 
-        if (x < 0 | (x % 10 == 0 && x != 0))
+        if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
 
         int rev = 0;
