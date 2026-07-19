d2fdf89840cc549233d4c316225204ed11268fcc
rsofficial2006-sys <rsofficial2006@gmail.com>
2026-07-19 15:06:00 +0530

    Time: 4 ms (16.59%) | Memory: 21.8 MB (38.92%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = nums.size();
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans ^= i;
+            ans ^= nums[i];
+        }
+
+        return ans;
+    }
+};
