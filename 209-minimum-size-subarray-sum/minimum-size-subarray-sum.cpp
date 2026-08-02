5a73e8d3f8e84dc46290412a42b770be9146ddd6
rsofficial2006-sys <rsofficial2006@gmail.com>
2026-08-02 12:55:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (5.01%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-
-    int n = nums.size();
+    int minSubArrayLen(int target, vector<int>& nums) {                                                                                                                     int n=nums.size();
     int left=0;
     int sum=0;
-    int ans= INT_MAX;
+    int ans=INT_MAX;
     for(int right=0;right<n;right++){
         sum+=nums[right];
         while(sum>=target){
@@ -13,7 +11,9 @@ public:
             sum-=nums[left];
             left++;
         }
-    }   
-    return(ans==INT_MAX)?0:ans; 
+
+    }
+
+    return(ans==INT_MAX)?0:ans;
     }
 };
